8b7f78820b6e848da2fb46841f3273ed97615a19
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-06-05 18:10:13 +0500

    PythonBot

---
+++ PYTHONbot.py
@@ -0,0 +1,54 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage, HumanMessage
+
+
+load_dotenv()
+api_key = os.getenv("BotApi")
+
+model = ChatGroq(model="llama-3.3-70b-versatile", api_key=api_key)
+
+
+system_prompt = SystemMessage(
+    content="""
+    You are a helpful assistant specialized only in Python.
+    You were built by Muhammad Hassan, an Artificial Intelligence developer.
+    You only respond to questions related to Python programming (e.g., syntax, libraries, debugging, errors, tips).
+    also make your answer simple and user friendly which is easily readable
+    If a question is not related to Python, politely respond that you only handle Python-related questions.
+    """
+)
+
+# Streamlit 
+st.title("🐍 Python ChatBot")
+
+# chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# Display past chat messages using the chat interface
+for msg in st.session_state.chat_history:
+    with st.chat_message("user" if isinstance(msg, HumanMessage) else "assistant"):
+        st.markdown(msg.content)
+
+# Chat input at the bottom
+user_input = st.chat_input("Ask a Python question...")
+
+if user_input:
+    # Show user message
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    # Add user message to history
+    st.session_state.chat_history.append(HumanMessage(content=user_input))
+
+    # Get bot response
+    response = model.invoke([system_prompt] + st.session_state.chat_history)
+
+    # Show bot message
+    with st.chat_message("assistant"):
+        st.markdown(response.content)
+
+    st.session_state.chat_history.append(response)
---
+++ pybot.py
@@ -0,0 +1,37 @@
+from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage, HumanMessage
+import streamlit as st
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+api_key=os.getenv("BotApi")
+
+model=ChatGroq(model="llama-3.3-70b-versatile", api_key=api_key)
+
+
+system_prompt=SystemMessage(
+    content=(
+        '''
+        You are a helpful assistant specializied only in python.You are design only responsd to teh querries 
+        that are related to the python prgramming language including syntax, libraries, errors, debugging, and best practices.
+        If a question is not related to the python just poiltely resposne them that you are design to answer only python questions
+'''
+    )
+)
+
+
+def get_response(query):
+    response=model.invoke([
+        system_prompt , HumanMessage(query)
+    ])
+    return response.content
+
+while True:
+    query=input("Enter your query: ")
+    if query=="exit":
+        break
+    print(get_response(query))
+
+
